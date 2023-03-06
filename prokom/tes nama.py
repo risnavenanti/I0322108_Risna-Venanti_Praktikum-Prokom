1dcdb58c370b7371a46428ff8125452bfc02ea9f
risna <risnavenanti@student.uns.ac.id>
2023-03-06 10:03:58 +0700

    commit pertama

---
+++ prokom/tes nama.py
@@ -0,0 +1,13 @@
+nama_depan  = "Risna"
+nama_belakang = "Risna Venanti"
+
+angka = 10
+desimal = 0.5
+boolean = True
+kelas_D = ["Risna", "kelas D", "dll"]
+dictionary = {"nama": "Risna", "kelas": "KLS D"}
+
+print("nama_depan")
+print("nama saya adalah Risna")
+
+print(f"nama saya adalah {nama_depan} (nama_belakang) dari kelas {data['kelas']}")
